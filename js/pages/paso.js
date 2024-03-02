5c5dfbb4fe522de0dba595f6d0ea3142f00a6425
Manuel Cecarelli <manuelcecarelli@gmail.com>
2024-03-02 16:39:58 -0300

    Avances varios

--- js/pages/paso.js
+++ js/pages/paso.js
@@ -1,15 +1,17 @@
-const tipoEleccion = 1;
-const tipoRecuento = 1;
+const tipoEleccion = 1; //PASO
+const tipoRecuento = 1; //Las respuestas de la API no contenían recuento definitivo.
 let banderaAnio = false;
 let banderaCargo = false;
+let consultaPeriodos;
+let consultaCargos;
 
 //Apenas se carga la página se piden los períodos a la API:
-let consultaPeriodos = fetch("https://resultados.mininterior.gob.ar/api/menu/periodos")
-    //Respuesta satisfactoria
+consultaPeriodos = fetch("https://resultados.mininterior.gob.ar/api/menu/periodos")
+    //Línea para espuesta satisfactoria
     .then(response => response.json())
+    //Línea por si se produce un error
     .catch(error =>
     {
-        //Se produce un error
         console.error(error); //Muestro el mensaje de error
     });
 
@@ -39,10 +41,16 @@ function detectarAnio()
     pedirCargos(selectAnio);
 };
 
-function detectarCargo()
+function detectarCargo(respuestaCargos)
 {
+    if (banderaCargo)
+    {
+        limpiarSelectDistrito();
+    }
     banderaCargo = true;
     console.log("El valor de la variable banderaCargo ahora es: " + banderaCargo);
+    let selectCargo = document.getElementById("select-cargo");
+    rellenarComboDistrito(selectCargo);
 }
 
 //Esta función es para renovar el combo de cargo y que no se acumulen cargos.
@@ -74,7 +82,7 @@ function limpiarSelectCargo() //En realidad intercambiamos select existente por 
 function pedirCargos(anio)
 {
     //pedimos los cargos a la API para el año seleccionado:
-    let consultaCargos = fetch("https://resultados.mininterior.gob.ar/api/menu?año=" + anio.value)
+    consultaCargos = fetch("https://resultados.mininterior.gob.ar/api/menu?año=" + anio.value)
         //Respuesta satisfactoria
         .then(response => response.json())
         .catch(error =>
@@ -100,3 +108,54 @@ function pedirCargos(anio)
                 }
             }))
 };
+
+//Función que rellena el combo "Distrito" cuando el usuario selecciona un cargo.
+function rellenarComboDistrito(selectCargo)
+{
+    consultaCargos.then(data =>  
+        data.forEach(indice =>
+            {
+                if (indice.IdEleccion == tipoEleccion)
+                {
+                    indice.Cargos.forEach(cargo =>
+                        {
+                            if (cargo.Cargo == selectCargo.value)
+                            {
+                                cargo.Distritos.forEach(distrito =>
+                                {
+                                    let selectDistrito = document.getElementById("select-distrito");
+                                    let nuevaOption = document.createElement('option');
+                                    nuevaOption.text = distrito.Distrito;
+                                    nuevaOption.value = distrito.Distrito;
+                                    selectDistrito.appendChild(nuevaOption);
+                                })
+                                
+                            }
+                        })
+                }
+            }))
+}
+
+function limpiarSelectDistrito()
+{
+    //identificamos nodo padre e hijo
+    let elementoPadre = document.getElementById("menu-page");
+    let antiguoHijo = document.getElementById("select-distrito");
+
+    //creamos el nuevo nodo que reemplazará al existente
+    let nuevoHijo = document.createElement("select");
+    let mismoContenido = document.createElement("option");
+    nuevoHijo.appendChild(mismoContenido);
+    mismoContenido.text = "Distrito";
+    mismoContenido.setAttribute("disabled", "true");
+    mismoContenido.setAttribute("selected", "true");
+    
+    //reemplazamos el antiguo por el nuevo
+    elementoPadre.replaceChild(nuevoHijo, antiguoHijo);
+
+    //Seteamos los atributos para que aplique correctamente el estilo CSS
+    nuevoHijo.setAttribute("class", "desplegable");
+    nuevoHijo.setAttribute("name", "select-distrito");
+    nuevoHijo.setAttribute("id", "select-distrito");
+    //nuevoHijo.setAttribute("onchange", "detectarCargo()");
+}
